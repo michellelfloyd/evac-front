db14ead860ad49bab5edd5fac6cb1e02acb1925c
Alex Brown <brown.alex.k@gmail.com>
2014-10-27 20:33:17 -0600

    working on maps

--- app/route-planner/route-planner.js
+++ app/route-planner/route-planner.js
@@ -2,13 +2,24 @@
 
 angular.module('myApp.route-planner', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/route-planner', {
-    templateUrl: 'route-planner/route-planner.html',
-    controller: 'RoutePlannerCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/route-planner', {
+            templateUrl: 'route-planner/route-planner.html',
+            controller: 'RoutePlannerCtrl'
+        });
+    }])
 
-.controller('RoutePlannerCtrl', [function() {
+    .controller('RoutePlannerCtrl', ['$scope', 'GoogleMapApi'.ns(), function ($scope, GoogleMapApi) {
 
-}]);
+        $scope.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 8
+        };
+
+        GoogleMapApi.then(function (maps) {
+            console.log(maps);
+        });
+    }]);
