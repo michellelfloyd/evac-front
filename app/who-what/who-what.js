5832e3c4d05f7bcd50e3d5138003e88a7ab01ab8
Matt Jensen <mjensen6889@gmail.com>
2014-10-28 20:30:47 -0600

    Styling changes to have label move right for emphasis when hovering

--- app/who-what/who-what.js
+++ app/who-what/who-what.js
@@ -13,19 +13,6 @@ angular.module('myApp.who-what', ['ngRoute'])
 
         $scope.oneAtATime = true;
 
-        $scope.groups = [
-            {
-                title: 'Dynamic Group Header - 1',
-                content: 'Dynamic Group Body - 1'
-            },
-            {
-                title: 'Dynamic Group Header - 2',
-                content: 'Dynamic Group Body - 2'
-            }
-        ];
-
-        $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
         $scope.addItem = function () {
             var newItemNo = $scope.items.length + 1;
             $scope.items.push('Item ' + newItemNo);
@@ -34,17 +21,6 @@ angular.module('myApp.who-what', ['ngRoute'])
         $scope.status = {
             isFirstOpen: true,
             isFirstDisabled: false
-        };
-
-        $scope.tabs = [
-            { title: 'Dynamic Title 1', content: 'Dynamic content 1' },
-            { title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true }
-        ];
-
-        $scope.alertMe = function () {
-            setTimeout(function () {
-                alert('You\'ve selected the alert tab!');
-            });
         };
 
     }]);
