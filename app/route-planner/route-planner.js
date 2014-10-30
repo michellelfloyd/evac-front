c7f27ccba56a6685a43628a49e9b2120a1277be0
Alex Brown <brown.alex.k@gmail.com>
2014-10-29 20:39:21 -0600

    rt WORKON_HOME=$HOME/.virtualenvs map working with directions, route, directions panel, dragable directions, transportation type

--- app/route-planner/route-planner.js
+++ app/route-planner/route-planner.js
@@ -2,53 +2,86 @@
 
 angular.module('myApp.route-planner', ['ngRoute'])
 
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/route-planner', {
-      templateUrl: 'route-planner/route-planner.html',
-      controller: 'RoutePlannerCtrl'
-    });
-  }])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/route-planner', {
+            templateUrl: 'route-planner/route-planner.html',
+            controller: 'RoutePlannerCtrl'
+        });
+    }])
 
-  .controller('RoutePlannerCtrl', ['$scope', 'GoogleMapApi'.ns(), function ($scope, GoogleMapApi) {
+    .controller('RoutePlannerCtrl', ['$scope', 'GoogleMapApi'.ns(), function ($scope, GoogleMapApi) {
 
-    $scope.locations = [
-      'orem, ut',
-      'payson, ut'
-    ];
+        $scope.locations = [
+            'orem, ut',
+            'payson, ut'
+        ];
 
-    GoogleMapApi.then(function (maps) {
-      $scope.maps = maps;
-      $scope.initialize();
-    });
+        GoogleMapApi.then(function (maps) {
+            $scope.maps = maps;
+            console.log(maps);
+            $scope.initialize();
+        });
 
-    $scope.initialize = function () {
-      $scope.directionsDisplay;
-      $scope.directionsService = new $scope.maps.DirectionsService();
-      $scope.map;
+        $scope.initialize = function () {
+            $scope.rendererOptions = {
+                draggable: true
+            };
+            $scope.directionsDisplay = new $scope.maps.DirectionsRenderer($scope.rendererOptions);;
+            $scope.directionsService = new $scope.maps.DirectionsService();
+            $scope.map;
+            $scope.travelModes = [
+                {
+                    name: "driving",
+                    value: $scope.maps.TravelMode.DRIVING
+                },
+                {
+                    name: "walking",
+                    value: $scope.maps.TravelMode.WALKING
+                },
+                {
+                    name: "bicycling",
+                    value: $scope.maps.TravelMode.BICYCLING
+                },
+                {
+                    name: "transit",
+                    value: $scope.maps.TravelMode.TRANSIT
+                }
 
-      $scope.directionsDisplay = new $scope.maps.DirectionsRenderer();
-      var chicago = new $scope.maps.LatLng(41.850033, -87.6500523);
-      var mapOptions = {
-        zoom: 7,
-        center: chicago
-      };
-      $scope.map = new $scope.maps.Map(document.getElementById('map-canvas'), mapOptions);
-      $scope.directionsDisplay.setMap($scope.map);
-    };
+            ]
 
-    $scope.calcRoute = function () {
-      var request = {
-        origin: $scope.start,
-        destination: $scope.end,
-        travelMode: $scope.maps.TravelMode.DRIVING
-      };
-      $scope.directionsService.route(request, function (response, status) {
-        if (status == $scope.maps.DirectionsStatus.OK) {
-          $scope.directionsDisplay.setDirections(response);
-          $scope.directionsDisplay.setPanel(document.getElementById("directionsPanel"));
-        }
-      });
-    };
+
+            $scope.directionsDisplay = new $scope.maps.DirectionsRenderer($scope.rendererOptions);
+//            $scope.chicago = new $scope.maps.LatLng(41.850033, -87.6500523);
+            var mapOptions = {
+                zoom: 7
+            };
+            $scope.map = new $scope.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+            $scope.directionsDisplay.setMap($scope.map);
+            $scope.directionsDisplay.setPanel(document.getElementByID("directionsPanel"));
+            $scope.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
+                computeTotalDistance(directionsDisplay.directions);
+            });
+
+            calcRoute()
+        };
+
+        $scope.calcRoute = function () {
+            var request = {
+                origin: $scope.start,
+                destination: $scope.end,
+//                travelMode: $scope.maps.TravelMode.DRIVING
+                travelMode: $scope.travelMode
+            };
+            $scope.directionsService.route(request, function (response, status) {
+                if (status == $scope.maps.DirectionsStatus.OK) {
+                    $scope.directionsDisplay.setDirections(response);
+                    $scope.directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+                }
+            });
+
+            $scope.maps.event.trigger($scope.map, 'resize');
+        };
 
 
 //    GoogleMapApi.then(function (maps) {
@@ -89,4 +122,4 @@ angular.module('myApp.route-planner', ['ngRoute'])
 //    });
 
 
-  }]);
+    }]);
