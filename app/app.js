8d30ea58f6734e72580d82fcd165630f3e26e11f
codingchick <heather@codingchick.com>
2014-10-30 18:43:17 -0600

    changes to the destination html page and app.js file

--- app/app.js
+++ app/app.js
@@ -2,29 +2,29 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'google-maps'.ns(),
-  'myApp.route-planner',
-  'myApp.version',
-  'ui.bootstrap',
-    'myApp.who-what',
-    'myApp.view2',
-    'myApp.version',
-    'ui.bootstrap',
-    'myApp.destination'
-    'myApp.people',
-    'myApp.pets',
-    'myApp.documents',
-    'myApp.valuables',
-    'myApp.travelTools',
-    'myApp.emergency'
-]).
-config(['$routeProvider', 'GoogleMapApiProvider'.ns(), function($routeProvider, GoogleMapApi) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+        'ngRoute',
+        'google-maps'.ns(),
+        'myApp.route-planner',
+        'myApp.version',
+        'ui.bootstrap',
+        'myApp.who-what',
+        'myApp.view2',
+        'myApp.version',
+        'ui.bootstrap',
+        'myApp.destination',
+        'myApp.people',
+        'myApp.pets',
+        'myApp.documents',
+        'myApp.valuables',
+        'myApp.travelTools',
+        'myApp.emergency'
+    ]).
+    config(['$routeProvider', 'GoogleMapApiProvider'.ns(), function ($routeProvider, GoogleMapApi) {
+        $routeProvider.otherwise({redirectTo: '/view1'});
 
         GoogleMapApi.configure({
             key: 'AIzaSyBgI1EKHNT3ArARMd9U9Ya4JYtHomJKL4E',
             v: '3.17',
             libraries: 'weather,geometry,visualization'
         });
-}]);
+    }]);
