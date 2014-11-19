7e6049d64206b51104adc92d8890999820d15dd1
Matt Jensen <mjensen6889@gmail.com>
2014-11-18 20:55:44 -0700

    in process of moving add-person to people view

--- app/people/people.js
+++ app/people/people.js
@@ -2,18 +2,23 @@
 
 angular.module('myApp.people', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/people', {
-    templateUrl: 'people/people.html',
-    controller: 'PeopleCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/people', {
+            templateUrl: 'people/people.html',
+            controller: 'PeopleCtrl'
+        });
+    }])
 
-.controller('PeopleCtrl', ['$scope', 'Restangular', function($scope, Restangular) {
+    .controller('PeopleCtrl', ['$scope', 'Restangular', function ($scope, Restangular) {
 
-    Restangular.all('people/').getList().then(function(people) {
-      $scope.people = people;
+        $scope.addPerson = "add-person/add-person.html";
 
-    });
+        $scope.getPeople = function () {
+            Restangular.all('people/').getList().then(function (people) {
+                $scope.people = people;
+            });
+        };
 
-}]);
+        $scope.getPeople();
+
+    }]);
