2608b1b974521a876586601325c655a2c7217198
Michelle Floyd <michellelfloyd@yahoo.com>
2014-12-11 20:58:41 -0700

    User auth working

--- app/User/user.js
+++ app/User/user.js
@@ -1,41 +1,25 @@
 'use strict';
 
-angular.module('myApp.user', ['ngRoute'])
+angular.module('myApp.user', ['ngRoute', 'ngCookies'])
 
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/login', {
       templateUrl: 'user/user.html',
       controller: 'UserCtrl'
     });
-//    $routeProvider.when('/documents', {
-//      templateUrl: 'documents/documents.html',
-//      controller: 'DocumentsCtrl'
-//    });
   }])
 
-  .controller('UserCtrl', ['$scope', function ($scope) {
+  .controller('UserCtrl', ['$scope', 'UserService', '$location',
+    function ($scope, UserService, $location) {
+    $scope.login = function(){
+      UserService.attemptLogin($scope.user).then(function(user){
+        console.log(user);
+        $location.path('/people')
+      });
+    };
 
-  }])
-  .directive('login', function () {
-    return {
-      restrict: 'A',
-      template: " <form> " +
-        "<label>Username</label>" +
-        "<input type='text' ng-model='username'>" +
-        "<label>Password</label>" +
-        "<input type='password' ng-model='password'>" +
-        "<br>" +
-        "<input type='submit'>" +
-        "</form>",
-      link: function (scope, elem, attrs) {
+  }]);
 
-        elem.bind('submit', function () {
 
-          var user_data = {
-            "username": scope.username,
-            "password": scope.password
-          };
-        })
-      }
-    }
-  })
+
+
